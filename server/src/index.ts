097ccec3262ca3a4ff48619fd9fce9b968e58229
Kush Gupta <Kushg827@gmail.com>
2025-11-03 18:49:28 +0530

    Basic project setup and error-codes implemented

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express, { Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { Env } from "./config/env.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { HTTPSTATUS } from "./config/http.config";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: Env.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get(
+  "/health",
+  asyncHandler(async (req: Request, res: Response) => {
+    res.status(HTTPSTATUS.OK).json({
+      message: "Server is healthy",
+      status: "OK",
+    });
+  })
+);
+
+app.listen(Env.PORT, async () => {
+  console.log(`Server running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
+});
---
+++ server/src/utils/get-env.ts
@@ -0,0 +1,6 @@
+export const getEnv = (key: string, defaultValue: string = "") => {
+    const val = process.env[key] ?? defaultValue;
+    if (!val)
+        throw new Error("Missing env variables " + key);
+    return val;
+}
