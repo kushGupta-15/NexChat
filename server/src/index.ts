c30e3bf0a7d799991e821be1c8ee286bf7686986
Kush Gupta <Kushg827@gmail.com>
2025-11-03 22:04:49 +0530

    Setup mongodb database and added chat, message and user model

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,9 @@ import cors from "cors";
 import { Env } from "./config/env.config";
 import { asyncHandler } from "./middlewares/asyncHandler.middleware";
 import { HTTPSTATUS } from "./config/http.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import connectDatabase from "./config/database.config";
+
 
 
 const app = express();
@@ -30,6 +33,9 @@ app.get(
   })
 );
 
+app.use(errorHandler);
+
 app.listen(Env.PORT, async () => {
+  await connectDatabase();
   console.log(`Server running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
 });
